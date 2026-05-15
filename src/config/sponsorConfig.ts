e18abb35cfb8b67a43cf26713f9a8e012b3bf18d
nairain <nairain521@gmail.com>
2026-05-15 16:25:00 +0800

    fix: 利用biome修正代码

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -9,7 +9,7 @@ export const sponsorConfig: SponsorConfig = {
 
 	// 赞助用途说明
 	usage:
-	    "您的赞助将用于服务器维护、内容创作和功能开发，帮助我持续提供优质内容。",
+		"您的赞助将用于服务器维护、内容创作和功能开发，帮助我持续提供优质内容。",
 
 	// 是否显示赞助者列表
 	showSponsorsList: true,
@@ -37,22 +37,6 @@ export const sponsorConfig: SponsorConfig = {
 			qrCode: "/assets/images/sponsor/wechat.png",
 			link: "",
 			description: "使用 微信 扫码赞助",
-			enabled: false,
-		},
-		{
-			name: "ko-fi",
-			icon: "simple-icons:kofi",
-			qrCode: "",
-			link: "https://ko-fi.com/cuteleaf",
-			description: "Buy a Coffee for Firefly",
-			enabled: false,
-		},
-		{
-			name: "爱发电",
-			icon: "simple-icons:afdian",
-			qrCode: "",
-			link: "https://ifdian.net/a/cuteleaf",
-			description: "通过 爱发电 进行赞助",
 			enabled: false,
 		},
 		{
