246a7ff40aeffe66d7757f21fc78e154890c7c81
nairain <nairain521@gmail.com>
2026-05-21 16:47:11 +0800

    Merge branch 'master' of https://github.com/CuteLeaf/Firefly

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "development") {
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.site_url,
-
+	
 	base: "/",
 	trailingSlash: "always",
 
